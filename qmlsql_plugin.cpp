d04d0085bb3a61ae5cdbd84fb2939d5e7b7ae36b
Pierre Ducroquet <pinaraf@pinaraf.info>
2014-04-13 02:47:34 +0200

    Fix the way we register the global object

--- qmlsql_plugin.cpp
+++ qmlsql_plugin.cpp
@@ -6,9 +6,12 @@
 
 #include <qqml.h>
 #include <QQmlEngine>
+#include <QQmlContext>
+#include <QDebug>
 
 void QmlSqlPlugin::registerTypes(const char *uri)
 {
+    qDebug() << "registerTypes ? adding database types";
     // @uri com.sql
     qmlRegisterType<SqlQueryModel>(uri, 1, 0, "SqlQueryModel");
     qmlRegisterType<SqlQueryBinding>(uri, 1, 0, "SqlQueryBinding");
@@ -17,5 +20,7 @@ void QmlSqlPlugin::registerTypes(const char *uri)
 
 void QmlSqlPlugin::initializeEngine(QQmlEngine *engine, const char *uri)
 {
-    engine->globalObject().setProperty("database", engine->newQObject(new SqlDatabase(this)));
+    Q_UNUSED(uri);
+    qDebug() << "Initializing engine ? adding database property";
+    engine->rootContext()->setContextProperty("database", new SqlDatabase(engine));
 }
